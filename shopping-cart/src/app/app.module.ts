1bc447121675dc256d3b47404fcba273bbb00ff4
shantanu.sadar <shantanu.sadar@cognizant.com>
2019-11-04 22:44:34 +0530

    added new

--- shopping-cart/src/app/app.module.ts
+++ shopping-cart/src/app/app.module.ts
@@ -6,11 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsComponent } from './products/products.component';
 import { MatGridListModule } from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
 import { OrderComponent } from './order/order.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const appRoutes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'orderHistory', component: OrderComponent },
+];
 
 @NgModule({
   declarations: [
@@ -24,8 +29,13 @@ import { OrderComponent } from './order/order.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatGridListModule,
+    MatListModule,
     MatCardModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- shopping-cart/src/app/order-history.service.ts
+++ shopping-cart/src/app/order-history.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpHeaders } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 
-const HEADER = { headers: new HttpHeaders({ 'Authentication': 'AFDSSASDF123512' }) };
 
 export interface Order {
   id: Number,
@@ -23,7 +21,7 @@ export class OrderHistoryService {
 
   constructor(private http: HttpClient) { }
   getOrdersHistory() {
-    return this.http.get<Order[]>('http://localhost:4000/orders');
+    return this.http.get<Order[]>('http://localhost:3000/orders');
 
   }
 
